c112016a4840e0469b6a2b7f52252e5979a79fce
Aric Johnson <aric87@comcast.net>
2015-09-04 15:30:31 -0400

    update js with event handlers

--- getTweets.js
+++ getTweets.js
@@ -3,6 +3,12 @@
 // view user http://twitter.com/"+ {user screen_name}
 
 $(document).ready(function(){
+    //bind event handlers
+    $('buttons a').click(function(e){e.preventDefault()});
+    $('.showRetweets').click(function(){});
+    $('.showFavorited').click(function(){});
+    $('.showAll').click(function(){});
+    // get JSON data
     $.getJSON('/data.json',function(response){
 
     });
