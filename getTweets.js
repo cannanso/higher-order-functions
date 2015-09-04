be1aa1ac3cd1b7949f06ea8f7b1da555dc0c27ad
Aric Johnson <aric87@comcast.net>
2015-09-04 15:10:27 -0400

    basic setup

---
+++ getTweets.js
@@ -0,0 +1,10 @@
+//url constructor http://twitter.com/"+{users screename}+"/status/"+{tweet id_str}
+//hashtag search url  http://twitter.com/search?q=%23" + {hashtag text}
+// view user http://twitter.com/"+ {user screen_name}
+
+$(document).ready(function(){
+    $.getJSON('/data.json',function(response){
+
+    });
+});
+
