7b135c671f142533f41a47cc72324435db12583f
Claire Annan <claire.annan.me@gmail.com>
2015-11-03 17:13:37 -0500

    Function formatTweets().

--- getTweets.js
+++ getTweets.js
@@ -2,6 +2,14 @@
 //hashtag search url  http://twitter.com/search?q=%23" + {hashtag text}
 //view user http://twitter.com/"+ {user screen_name}
 
+function formatTweet(data){
+    return '<div class="tweet">'+
+            '<p>' + data.text + '</p>'+
+            '<p class="favorites">favorites <span>'+ data.favorite_count + '</span></p><p class="retweets">retweets <span>' + data.retweet_count + '</span></p>'+
+            '<a href="http://twitter.com/'+ data.user.screen_name + '/status/"'+ data.id_str +'" class="readMore">View on twitter</a>'+
+            '</div>';
+}
+
 $(document).ready(function(){
 	var tweets = [];
 	var retweets = [];
@@ -20,28 +28,30 @@ $(document).ready(function(){
     	$('.showTweets').html(tweets);
     });
     // get JSON data
+//http://twitter.com/"+{users screename}+"/status/"+{tweet id_str}
+
     $.getJSON('data.json',function(response){
    		tweets = response.map(function(data){
-    		return '<div class="tweet"><p>'+ data.text + '</p></div>';
+    		return formatTweet(data);
     	});
 
     	retweets = response.filter(function(element){
     		return element.retweeted;
     	}).map(function(data){
-            return '<div class="tweet"><p>'+ data.text + '</p></div>';
+            return formatTweet(data);
         });
 
         favorites = response.filter(function(element){
             return element.favorited;
         }).map(function(data){
-            return '<div class="tweet"><p>'+ data.text + '</p></div>';
+            return formatTweet(data);
         });
 
         totalRetweets = response.map(function(data){
             return data.retweet_count;
         }).reduce(function(total, num){
             return total + num;
-        });
+        }, 0);
 
 	    $('.showTweets').append(tweets);
         $('.showRetweets').append(' (' + totalRetweets + ')');
